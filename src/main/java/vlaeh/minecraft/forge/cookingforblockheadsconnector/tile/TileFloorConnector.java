0329324ff489cc1f4cb45f82d25086b3056da922
Vlaeh <Vlaeh@users.noreply.github.com>
2018-03-26 18:50:58 +0200

    Updating connector block to handle dynamic item handlers

--- src/main/java/vlaeh/minecraft/forge/cookingforblockheadsconnector/tile/TileFloorConnector.java
+++ src/main/java/vlaeh/minecraft/forge/cookingforblockheadsconnector/tile/TileFloorConnector.java
@@ -1,5 +1,6 @@
 package vlaeh.minecraft.forge.cookingforblockheadsconnector.tile;
 
+import net.blay09.mods.cookingforblockheads.api.capability.CapabilityKitchenConnector;
 import net.blay09.mods.cookingforblockheads.api.capability.CapabilityKitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.api.capability.CapabilityKitchenSmeltingProvider;
 import net.blay09.mods.cookingforblockheads.api.capability.KitchenItemProvider;
@@ -15,8 +16,8 @@ import javax.annotation.Nullable;
 
 public class TileFloorConnector extends TileEntity {
 
-    private final ItemStackHandler emptyItemHandler = new ItemStackHandler(0) {};
-    private final KitchenItemProvider itemProvider = new KitchenItemProvider(emptyItemHandler);
+    private final static ItemStackHandler emptyItemHandler = new ItemStackHandler(0) {};
+    private final MyKitchenItemProvider itemProvider = new MyKitchenItemProvider(emptyItemHandler);
 
     @Override
     public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
@@ -24,14 +25,18 @@ public class TileFloorConnector extends TileEntity {
                 || super.hasCapability(capability, facing);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
         if(capability == CapabilityKitchenItemProvider.CAPABILITY) {
             final BlockPos position = pos.offset(EnumFacing.UP);
             final TileEntity tileEntity = world.getTileEntity(position);
+            if (itemProvider.lastTileEntity == tileEntity)
+                return (T) itemProvider.refreshSize();
             if (tileEntity != null) {
                 if (tileEntity.hasCapability(CapabilityKitchenItemProvider.CAPABILITY, null)
-                    || tileEntity.hasCapability(CapabilityKitchenSmeltingProvider.CAPABILITY, null)) {
+                    || tileEntity.hasCapability(CapabilityKitchenSmeltingProvider.CAPABILITY, null)
+                    || tileEntity.hasCapability(CapabilityKitchenConnector.CAPABILITY, null)) {
                     itemProvider.setItemHandler(emptyItemHandler); // avoid scanning twice CookingForBlockheads blocks
                 } else {
                     final IItemHandler c = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
@@ -45,6 +50,32 @@ public class TileFloorConnector extends TileEntity {
             return (T) itemProvider;
         }
         return super.getCapability(capability, facing);
+    }
+
+    private final static class MyKitchenItemProvider extends KitchenItemProvider {
+        TileEntity lastTileEntity = null;
+        IItemHandler lastItemHandler = null;
+        int lastSize = -1;
+
+        public MyKitchenItemProvider(final IItemHandler itemHandler) {
+            setItemHandler(itemHandler);
+        }
+
+        @Override
+        public void setItemHandler(final IItemHandler itemHandler) {
+            lastItemHandler = itemHandler;
+            lastSize = itemHandler.getSlots();
+            super.setItemHandler(itemHandler);
+        }
+
+        public MyKitchenItemProvider refreshSize() {
+            final int newSize = lastItemHandler.getSlots();
+            if (lastSize < newSize) {
+                lastSize = newSize;
+                super.setItemHandler(lastItemHandler);
+            }
+            return this;
+        }
     }
 
 }
